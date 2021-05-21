c545f93f49f57e95562b3447cf069d94588a78be
HPatel000 <patelheli1711@gmail.com>
2021-05-21 17:04:30 +0530

    server change

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const spotifyWebApi = require('spotify-web-api-node')
+const path = require('path')
 
 const app = express()
 app.use(cors())
